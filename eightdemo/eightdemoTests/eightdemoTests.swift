9fb6099995cfbe2fd1a9949cc9bde48707a11aa7
zhangsoul <50726181+zhangsoul@users.noreply.github.com>
2019-05-22 17:38:41 +0800

    列表头部视图跟随滑动变大

---
+++ eightdemo/eightdemoTests/eightdemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  eightdemoTests.swift
+//  eightdemoTests
+//
+//  Created by jujiakeji on 2019/5/22.
+//  Copyright © 2019年 Soul. All rights reserved.
+//
+
+import XCTest
+@testable import eightdemo
+
+class eightdemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
